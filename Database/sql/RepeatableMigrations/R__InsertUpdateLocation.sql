033fc72445b5c2528a711f5f765f65f954d98e87
James Cockayne <james.cockayne@greenhowconsulting.com>
2023-12-05 16:37:09 +0000

    Fix bug where duplicate locations could sometimes be recorded.

--- Database/sql/RepeatableMigrations/R__InsertUpdateLocation.sql
+++ Database/sql/RepeatableMigrations/R__InsertUpdateLocation.sql
@@ -26,18 +26,19 @@ begin
 		latitude,
 		longitude
 	)
-	select
-		l.address_1,
-		l.address_2,
-		l.city,
-		l.state,
-		l.zip,
-		l.county,
-		l.location_source_value,
-		l.country_concept_id,
-		l.country_source_value,
-		l.latitude,
-		l.longitude
+	select 
+		distinct
+			l.address_1,
+			l.address_2,
+			l.city,
+			l.state,
+			l.zip,
+			l.county,
+			l.location_source_value,
+			l.country_concept_id,
+			l.country_source_value,
+			l.latitude,
+			l.longitude
 	from @Locations l
 	where not exists (
 		select *
