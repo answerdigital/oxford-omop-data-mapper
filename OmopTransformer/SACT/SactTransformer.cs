bf7ba3fdd732072ebe5b90a57cc3bcef4455d32e
James Cockayne <james.cockayne@greenhowconsulting.com>
2023-12-28 10:30:16 +0000

    Added support for migrating SACT persons.
    Former-commit-id: 32e62de77cf9f76e0b414d929598352aafcb894e

---
+++ OmopTransformer/SACT/SactTransformer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using OmopTransformer.Omop.Location;
+using OmopTransformer.Omop.Person;
+using OmopTransformer.SACT.Staging;
+using OmopTransformer.Transformation;
+
+namespace OmopTransformer.SACT;
+
+internal class SactTransformer : Transformer
+{
+    private readonly ILocationRecorder _locationRecorder;
+    private readonly IPersonRecorder _personRecorder;
+    
+    public SactTransformer(IRecordTransformer recordTransformer, ILogger<IRecordTransformer> logger, TransformOptions transformOptions, IRecordProvider recordProvider, ILocationRecorder locationRecorder, IPersonRecorder personRecorder) : base(recordTransformer, logger, transformOptions, recordProvider)
+    {
+        _locationRecorder = locationRecorder;
+        _personRecorder = personRecorder;
+    }
+
+    public async Task Transform(CancellationToken cancellationToken)
+    {
+        await Transform<Sact, SactPerson>(
+            _personRecorder.InsertUpdatePersons,
+            "SACT Person",
+            cancellationToken);
+
+        await Transform<Sact, SactLocation>(
+            _locationRecorder.InsertUpdateLocations,
+            "SACT Locations",
+            cancellationToken);
+    }
+}
---
+++ OmopTransformer/SACT/Staging/SactPerson.cs
@@ -0,0 +1,32 @@
+﻿using OmopTransformer.Annotations;
+using OmopTransformer.Omop.Person;
+using OmopTransformer.Transformation;
+
+namespace OmopTransformer.SACT.Staging;
+
+internal class SactPerson : OmopPerson<Sact>
+{
+    [CopyValue(nameof(Source.NHS_Number))]
+    public override string? person_source_value { get; set; }
+
+    [Transform(typeof(YearSelector), nameof(Source.Date_Of_Birth))]
+    public override int? year_of_birth { get; set; }
+
+    [Transform(typeof(MonthOfYearSelector), nameof(Source.Date_Of_Birth))]
+    public override int? month_of_birth { get; set; }
+
+    [Transform(typeof(DayOfMonthSelector), nameof(Source.Date_Of_Birth))]
+    public override int? day_of_birth { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.Date_Of_Birth))]
+    public override DateTime? birth_datetime { get; set; }
+
+    [ConstantValue(0, "Unknown concept")]
+    public override int? ethnicity_concept_id { get; set; }
+
+    [Transform(typeof(GenderLookup), nameof(Source.Person_Stated_Gender_Code))]
+    public override int? gender_source_concept_id { get; set; }
+
+    [CopyValue(nameof(Source.Person_Stated_Gender_Code))]
+    public override string? gender_source_value { get; set; }
+}
