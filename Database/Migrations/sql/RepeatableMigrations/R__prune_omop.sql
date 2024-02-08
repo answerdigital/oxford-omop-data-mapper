5bac7ba38d3579fc990b1046d89c2f8ed94f239e
James Cockayne <james.cockayne@greenhowconsulting.com>
2024-02-08 14:48:03 +0000

    Extend prune function to remove entries from more tables.

--- Database/Migrations/sql/RepeatableMigrations/R__prune_omop.sql
+++ Database/Migrations/sql/RepeatableMigrations/R__prune_omop.sql
@@ -9,12 +9,42 @@ create procedure cdm.prune_omop
 as
 begin
 
-delete
+declare @personsToDelete table
+(
+	person_id int not null
+);
+
+insert into @personsToDelete
+(
+	person_id
+)
+select
+	person_id
 from cdm.person
 where gender_concept_id is null
 	or person_source_value is null
 	or race_concept_id is null
 	or ethnicity_concept_id is null;
+
+delete co
+from cdm.condition_occurrence co
+	inner join @personsToDelete p
+		on co.person_id = p.person_id;
+
+delete vd
+from cdm.visit_detail vd
+	inner join @personsToDelete p
+		on vd.person_id = p.person_id;
+
+delete vo
+from cdm.visit_occurrence vo
+	inner join @personsToDelete p
+		on vo.person_id = p.person_id;
+
+delete p
+from cdm.person p
+	inner join @personsToDelete ptd
+		on p.person_id = ptd.person_id;
 
 delete l
 from cdm.location l
