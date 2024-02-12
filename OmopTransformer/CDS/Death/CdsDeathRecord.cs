4f69d939068b9f915de8f9f060b0d852569b63f4
James Cockayne <james.cockayne@greenhowconsulting.com>
2024-02-12 12:03:25 +0000

    Mark nullable fields as nullable.

--- OmopTransformer/CDS/Death/CdsDeathRecord.cs
+++ OmopTransformer/CDS/Death/CdsDeathRecord.cs
@@ -7,6 +7,6 @@ namespace OmopTransformer.CDS.Death;
 [SourceQuery("CdsDeath.xml")]
 internal class CdsDeathRecord
 {
-    public string nhs_number { get; set; }
-    public string death_date { get; set; }
-}
+    public string? nhs_number { get; set; }
+    public string? death_date { get; set; }
+}   
