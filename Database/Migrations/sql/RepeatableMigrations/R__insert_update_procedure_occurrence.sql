e12ad4d7256de6da15f898cefd443a245936061d
James Cockayne <james.cockayne@greenhowconsulting.com>
2024-02-16 14:48:50 +0000

    Partial procedure occurence implementation.

---
+++ Database/Migrations/sql/RepeatableMigrations/R__insert_update_procedure_occurrence.sql
@@ -0,0 +1,106 @@
+if object_id ('cdm.insert_update_procedure_occurrence') is not null
+begin
+drop procedure cdm.insert_update_procedure_occurrence;
+end
+go
+
+create procedure cdm.insert_update_procedure_occurrence
+	@rows cdm.procedure_occurrence_row readonly,
+	@DataSource varchar(20)
+as
+begin
+
+	declare @NewRecords as table
+	(
+		procedure_occurrence_id int
+	);
+	
+	insert into cdm.procedure_occurrence
+	(
+		person_id,
+		procedure_concept_id,
+		procedure_date,
+		procedure_datetime,
+		procedure_end_date,
+		procedure_end_datetime,
+		procedure_type_concept_id,
+		modifier_concept_id,
+		quantity,
+		provider_id,
+		visit_occurrence_id,
+		visit_detail_id,
+		procedure_source_value,
+		procedure_source_concept_id,
+		modifier_source_value,
+		RecordConnectionIdentifier
+	)
+	output inserted.procedure_occurrence_id
+	into @NewRecords (procedure_occurrence_id)
+	select
+		p.person_id,
+		r.procedure_concept_id,
+		r.procedure_date,
+		r.procedure_datetime,
+		r.procedure_end_date,
+		r.procedure_end_datetime,
+		r.procedure_type_concept_id,
+		r.modifier_concept_id,
+		r.quantity,
+		r.provider_id,
+		r.visit_occurrence_id,
+		r.visit_detail_id,
+		r.procedure_source_value,
+		r.procedure_source_concept_id,
+		r.modifier_source_value,
+		r.RecordConnectionIdentifier
+	from @rows r
+		inner join cdm.person p
+			on r.nhs_number = p.person_source_value
+	where 
+		not exists (
+			select	*
+			from cdm.procedure_occurrence vo
+			where vo.RecordConnectionIdentifier = r.RecordConnectionIdentifier
+		);
+
+
+	declare @columns table (Name varchar(max));
+
+	insert into @columns
+	(
+		Name
+	)
+	values
+	('person_id'),
+	('procedure_concept_id'),
+	('procedure_date'),
+	('procedure_datetime'),
+	('procedure_end_date'),
+	('procedure_end_datetime'),
+	('procedure_type_concept_id'),
+	('modifier_concept_id'),
+	('quantity'),
+	('provider_id'),
+	('visit_occurrence_id'),
+	('visit_detail_id'),
+	('procedure_source_value'),
+	('procedure_source_concept_id'),
+	('modifier_source_value'),
+	('RecordConnectionIdentifier');
+
+	insert into provenance
+	(
+		table_type_id,
+		table_key,
+		column_name,
+		data_source
+	)
+	select
+		32, --	procedure_occurrence
+		procedure_occurrence_id,
+		lc.name,
+		@DataSource
+	from @NewRecords rl
+	cross apply (select Name from @columns) lc;
+
+end
---
+++ OmopTransformer/Omop/ProcedureOccurrence/OmopProcedureOccurrence.cs
@@ -0,0 +1,24 @@
+﻿namespace OmopTransformer.Omop.ProcedureOccurrence;
+
+internal abstract class OmopProcedureOccurrence<T> : IOmopRecord<T>
+{
+    public virtual string? nhs_number { get; set; }
+    public virtual int[]? procedure_concept_id { get; set; }
+    public virtual DateTime? procedure_date { get; set; }
+    public virtual DateTime? procedure_datetime { get; set; }
+    public virtual DateTime? procedure_end_date { get; set; }
+    public virtual DateTime? procedure_end_datetime { get; set; }
+    public virtual int? procedure_type_concept_id { get; set; }
+    public virtual int? modifier_concept_id { get; set; }
+    public virtual int? quantity { get; set; }
+    public virtual int? provider_id { get; set; }
+    public virtual int? visit_occurrence_id { get; set; }
+    public virtual int? visit_detail_id { get; set; }
+    public virtual string? procedure_source_value { get; set; }
+    public virtual int? procedure_source_concept_id { get; set; }
+    public virtual string? modifier_source_value { get; set; }
+    public virtual string? RecordConnectionIdentifier { get; set; }
+
+    public string OmopTargetTypeDescription => "ProcedureOccurrence";
+    public T? Source { get; set; }
+}
