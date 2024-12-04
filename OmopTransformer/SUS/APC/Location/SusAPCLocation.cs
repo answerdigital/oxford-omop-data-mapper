4878b3e99adba03fa4bc65aea6ddb8f356b8fa99
Tom Holt <tom.holt@answerdigital.com>
2024-12-04 13:14:17 +0000

    add first sus location draft

---
+++ OmopTransformer/SUS/APC/Location/SusAPCLocation.cs
@@ -0,0 +1,10 @@
+﻿using OmopTransformer.Annotations;
+using OmopTransformer.Omop.Location;
+
+namespace OmopTransformer.SUS.APC.Location;
+
+internal class SusAPCLocation : OmopLocation<SusAPCLocationRecord>
+{
+    [CopyValue(nameof(Source.Postcode))]
+    public override string? location_source_value { get; set; }
+}
---
+++ OmopTransformer/SUS/APC/Location/SusAPCLocationRecord.cs
@@ -0,0 +1,13 @@
+﻿using OmopTransformer.Annotations;
+
+namespace OmopTransformer.SUS.APC;
+
+[DataOrigin("SUS")]
+[Description("SUS Inpatient Location")]
+[SourceQuery("SusAPCLocation.xml")]
+internal class SusAPCLocationRecord
+{
+    public string? Postcode { get; set; }
+    public string? NHSNumber { get; set; }
+
+}
