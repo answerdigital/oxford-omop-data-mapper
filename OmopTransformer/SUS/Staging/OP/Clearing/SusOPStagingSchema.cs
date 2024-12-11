9c3959c2b46f64861ce9d7c3f91efc47f2d60b12
James Cockayne <James.Cockayne@answerdigital.com>
2024-12-11 09:45:40 +0000

    Fix bug where SUS OP staging tables could not be cleared.

--- OmopTransformer/SUS/Staging/OP/Clearing/SusOPStagingSchema.cs
+++ OmopTransformer/SUS/Staging/OP/Clearing/SusOPStagingSchema.cs
@@ -12,10 +12,10 @@ internal class SusOPStagingSchema : StagingSchema, ISusOPStagingSchema
 
     protected override string[] ClearStagingSql =>
     [
-        "if object_id('omop_staging.sus_OP_ICDDiagnosis') is not null begin delete oomop_staging.sus_OP_ICDDiagnosis end;",
-        "if object_id('omop_staging.sus_OP_ReadDiagnosis') is not null begin delete oomop_staging.sus_OP_ReadDiagnosis end;",
-        "if object_id('omop_staging.sus_OP_OPCSProcedure') is not null begin delete oomop_staging.sus_OP_OPCSProcedure end;",
-        "if object_id('omop_staging.sus_OP_ReadProcedure') is not null begin delete oomop_staging.sus_OP_ReadProcedure end;",
-        "if object_id('omop_staging.sus_OP') is not null begin delete oomop_staging.sus_OP end;",
+        "if object_id('omop_staging.sus_OP_ICDDiagnosis') is not null begin delete omop_staging.sus_OP_ICDDiagnosis end;",
+        "if object_id('omop_staging.sus_OP_ReadDiagnosis') is not null begin delete omop_staging.sus_OP_ReadDiagnosis end;",
+        "if object_id('omop_staging.sus_OP_OPCSProcedure') is not null begin delete omop_staging.sus_OP_OPCSProcedure end;",
+        "if object_id('omop_staging.sus_OP_ReadProcedure') is not null begin delete omop_staging.sus_OP_ReadProcedure end;",
+        "if object_id('omop_staging.sus_OP') is not null begin delete omop_staging.sus_OP end;",
     ];
 }
