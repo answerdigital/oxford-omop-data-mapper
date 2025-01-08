3f6d7e4572d8824cdc9fbbed29099c31064d5917
Tom Holt <tom.holt@answerdigital.com>
2025-01-08 09:38:29 +0000

    initial observation

---
+++ OmopTransformer/SUS/OP/Observation/CarerSupportIndicator/SusOPCarerSupportIndicator.cs
@@ -0,0 +1,34 @@
+﻿using OmopTransformer.Annotations;
+using OmopTransformer.Omop.Observation;
+using OmopTransformer.Transformation;
+
+namespace OmopTransformer.SUS.OP.Observation.CarerSupportIndicator;
+
+[Notes(
+    "Notes",
+    "Observations do not require a standardized test or other activity to generate clinical fact. Typical observations are medical history, family history, lifestyle choices, healthcare utilization patterns, social circumstances etc",
+    "Valid Observation Concepts are not enforced to be from any domain.  They should still be standard concepts and typically belong to the Observation or Measurement domain.",
+    "Observations can be stored as attribute value pairs, with the attribute as the Observation Concept and the value representing the clinical fact. This fact can be stored as a Concept (value_as_concept), a numerical value (value_as_number) or a verbatim string (value_as_string)")]
+internal class SusOPCarerSupportIndicator : OmopObservation<SusOPCarerSupportIndicatorRecord>
+{
+    [CopyValue(nameof(Source.NHSNumber))]
+    public override string? nhs_number { get; set; }
+
+    [CopyValue(nameof(Source.GeneratedRecordIdentifier))]
+    public override string? RecordConnectionIdentifier { get; set; }
+
+    [ConstantValue(4224770, "Social support status")]
+    public override int? observation_concept_id { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? observation_date { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? observation_datetime { get; set; }
+
+    [ConstantValue(32834, "EHR outpatient note")]
+    public override int? observation_type_concept_id { get; set; }
+
+    [CopyValue(nameof(Source.CarerSupportIndicator))]
+    public override string? value_as_string { get; set; }
+}
---
+++ OmopTransformer/SUS/OP/VisitDetails/SusOPVisitDetailsRecord.cs
@@ -0,0 +1,21 @@
+﻿using OmopTransformer.Annotations;
+
+namespace OmopTransformer.SUS.APC.VisitDetails;
+
+[DataOrigin("SUS")]
+[Description("Sus Inptatient VisitDetails")]
+[SourceQuery("SusAPCVisitDetails.xml")]
+internal class SusAPCVisitDetailsRecord
+{
+    public string? NHSNumber { get; set; }
+    public string? GeneratedRecordIdentifier { get; set; }
+    public int? HospitalProviderSpellNumber { get; set; }
+    public string? VisitStartDate { get; set; }
+    public string? VisitStartTime { get; set; }
+    public string? VisitEndDate { get; set; }
+    public string? VisitEndTime { get; set; }
+    public int? VisitOccurrenceConceptId { get; set; }
+    public int? VisitTypeConceptId { get; set; }
+    public int? SourceOfAdmissionHospitalProviderSpell { get; set; }
+    public int? DischargeDestinationHospitalProviderSpell { get; set; }
+}
