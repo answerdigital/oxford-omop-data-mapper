28d2aeba83b44828a598f7f003ce4b6c265d6b16
James Cockayne <James.Cockayne@answerdigital.com>
2025-01-22 13:59:27 +0000

    Alter existing patch to remove generated with clause that is not supported for Oxford's database server.

--- Database/Migrations/sql/VersionedMigrations/V61__Update_spell_type.sql
+++ Database/Migrations/sql/VersionedMigrations/V61__Update_spell_type.sql
@@ -27,7 +27,7 @@ CREATE NONCLUSTERED INDEX [IDX_cdm_observation_person_id_observation_date_observ
 	[observation_concept_id] ASC,
 	[RecordConnectionIdentifier] ASC,
 	[HospitalProviderSpellNumber] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)
 GO
 
 
