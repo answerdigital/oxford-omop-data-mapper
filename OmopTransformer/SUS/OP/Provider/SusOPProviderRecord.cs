12047786613e851aa412d80b38a6f4ae20913efb
Tom Holt <tom.holt@answerdigital.com>
2025-01-23 16:08:47 +0000

    add op provider

---
+++ OmopTransformer/SUS/OP/Provider/SusOPProviderRecord.cs
@@ -0,0 +1,12 @@
+﻿using OmopTransformer.Annotations;
+
+namespace OmopTransformer.SUS.OP.Provider;
+
+[DataOrigin("SUS")]
+[Description("SUS Outpatient Provider")]
+[SourceQuery("SusOPProvider.xml")]
+internal class SusOPProviderRecord
+{
+    public string? ConsultantCode { get; set; }
+    public string? MainSpecialtyCode { get; set; }
+}
