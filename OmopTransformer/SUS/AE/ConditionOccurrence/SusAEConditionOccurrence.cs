cf9cb77f7b8553c50795e052861f36032390b39a
Tom Holt <tom.holt@answerdigital.com>
2025-02-04 11:58:34 +0000

    add initial procedure & condition mappings

---
+++ OmopTransformer/SUS/AE/ConditionOccurrence/SusAEConditionOccurrence.cs
@@ -0,0 +1,35 @@
+﻿using OmopTransformer.Annotations;
+using OmopTransformer.Omop.ConditionOccurrence;
+using OmopTransformer.Transformation;
+
+namespace OmopTransformer.SUS.AE.ConditionOccurrence;
+
+internal class SusAEConditionOccurrence : OmopConditionOccurrence<SusAEConditionOccurrenceRecord>
+{
+    [CopyValue(nameof(Source.NHSNumber))]
+    public override string? nhs_number { get; set; }
+
+    // [Transform(typeof(Icd10StandardNonStandardSelector), nameof(Source.AccidentAndEmergencyDiagnosis))]
+    // public override int? condition_source_concept_id { get; set; }
+
+    // [Transform(typeof(StandardConditionConceptSelector), useOmopTypeAsSource: true, nameof(condition_source_concept_id))]
+    // public override int? condition_concept_id { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? condition_start_date { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? condition_start_datetime { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? condition_end_date { get; set; }
+
+    [Transform(typeof(DateConverter), nameof(Source.CDSActivityDate))]
+    public override DateTime? condition_end_datetime { get; set; }
+    
+    [ConstantValue(32818, "EHR administration record")]
+    public override int? condition_type_concept_id { get; set; }
+
+    [CopyValue(nameof(Source.GeneratedRecordIdentifier))]
+    public override string? RecordConnectionIdentifier { get; set; }
+}
---
+++ OmopTransformer/SUS/AE/ConditionOccurrence/SusAEConditionOccurrenceRecord.cs
@@ -0,0 +1,14 @@
+﻿using OmopTransformer.Annotations;
+
+namespace OmopTransformer.SUS.AE.ConditionOccurrence;
+
+[DataOrigin("SUS")]
+[Description("SUS Inpatient Condition Occurrence")]
+[SourceQuery("SusAEConditionOccurrence.xml")]
+internal class SusAEConditionOccurrenceRecord
+{
+    public string? DiagnosisICD { get; set; }
+    public string? GeneratedRecordIdentifier { get; set; }
+    public string? NHSNumber { get; set; }
+    public string? CDSActivityDate { get; set; }
+}
