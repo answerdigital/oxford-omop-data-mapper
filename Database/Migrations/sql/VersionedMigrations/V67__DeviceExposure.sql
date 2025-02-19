1e984bd065dd2306aae9e4e3215347ecebbe176f
James Cockayne <James.Cockayne@answerdigital.com>
2025-02-19 15:51:27 +0000

    Mark device_exposure_end_date as nullable

--- Database/Migrations/sql/VersionedMigrations/V67__DeviceExposure.sql
+++ Database/Migrations/sql/VersionedMigrations/V67__DeviceExposure.sql
@@ -4,7 +4,7 @@ create type [cdm].[device_exposure_row] as table
     [device_concept_id] [int] NOT NULL,
     [device_exposure_start_date] [date] NOT NULL,
     [device_exposure_start_datetime] [datetime] NULL,
-    [device_exposure_end_date] [date] NOT NULL,
+    [device_exposure_end_date] [date] NULL,
     [device_exposure_end_datetime] [datetime] NULL,
     [device_type_concept_id] [int] NOT NULL,
     [unique_device_id] [varchar](50) NULL,
