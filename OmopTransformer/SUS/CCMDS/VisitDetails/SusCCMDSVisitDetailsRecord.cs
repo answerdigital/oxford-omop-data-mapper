ee16b7c0470b09d969632c8732c9f554e50d2f8d
Khayam Amin <khayam.amin@answerdigital.com>
2025-03-05 15:31:18 +0000

    CCMDS Visit Details Changes

---
+++ OmopTransformer/SUS/CCMDS/VisitDetails/SusCCMDSVisitDetailsRecord.cs
@@ -0,0 +1,16 @@
+﻿using OmopTransformer.Annotations;
+
+namespace OmopTransformer.SUS.CCMDS.VisitDetails;
+
+[DataOrigin("SUS")]
+[Description("Sus Critical Care VisitDetails")]
+[SourceQuery("SusCCMDSVisitDetails.xml")]
+internal class SusCCMDSVisitDetailsRecord
+{
+    public string? NHSNumber { get; set; }
+    public string? HospitalProviderSpellNumber { get; set; }
+    public string? VisitStartDate { get; set; }
+    public string? VisitStartTime { get; set; }
+    public string? VisitEndDate { get; set; }
+    public string? VisitEndTime { get; set; }
+}
