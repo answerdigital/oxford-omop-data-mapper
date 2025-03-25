9907a4d6d579c6c9f82629a8d4e818937bcf78a1
holtyad <tom.holt@answerdigital.com>
2025-03-25 09:33:03 +0000

    update cte naming

--- Database/Migrations/sql/VersionedMigrations/V73__RunMetricsView.sql
+++ Database/Migrations/sql/VersionedMigrations/V73__RunMetricsView.sql
@@ -1,16 +1,16 @@
 create or alter view [dbo].[run_analysis_metrics] as
-with magic as (
+with rank_provenance as (
     select
         data_source,
         substring(data_source, charindex(':', data_source) + 1, len(data_source)) as origin,
         row_number() over (partition by table_key, table_type_id order by column_name desc) as [rank]
     from dbo.provenance
 ),
-magic_counts as (
+rank_provenance_counts as (
     select
         origin,
         isnull(count(*), 0) as output_count
-    from magic
+    from rank_provenance
     where [rank] = 1
     group by origin
 ),
@@ -33,8 +33,7 @@ select
     end as output_count
 from dbo.run_analysis ra
 inner join latest_run lr on ra.run_id = lr.run_id
-left join magic_counts mc 
+left join rank_provenance_counts mc 
     on lower(replace(substring(ra.origin, charindex(':', ra.origin) + 1, len(ra.origin)), ' ', '')) = lower(replace(mc.origin, ' ', ''));
-
 
 go
