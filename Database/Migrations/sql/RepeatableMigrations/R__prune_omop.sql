c658b0dc58d2cb4babc8d2571811f8cd6f200e07
James Cockayne <James.Cockayne@answerdigital.com>
2025-04-01 13:33:00 +0100

    Rerrange delete statements to satisfy foreign key

--- Database/Migrations/sql/RepeatableMigrations/R__prune_omop.sql
+++ Database/Migrations/sql/RepeatableMigrations/R__prune_omop.sql
@@ -39,16 +39,6 @@ from cdm.condition_occurrence co
 	inner join @personsToDelete p
 		on co.person_id = p.person_id;
 
-delete vd
-from cdm.visit_detail vd
-	inner join @personsToDelete p
-		on vd.person_id = p.person_id;
-
-delete vo
-from cdm.visit_occurrence vo
-	inner join @personsToDelete p
-		on vo.person_id = p.person_id;
-
 delete m
 from cdm.measurement m
 	inner join @personsToDelete p
@@ -68,6 +58,21 @@ delete de
 from cdm.drug_exposure de
 	inner join @personsToDelete p
 		on de.person_id = p.person_id;
+
+delete de
+from cdm.device_exposure de
+	inner join @personsToDelete p
+		on de.person_id = p.person_id;
+
+delete vd
+from cdm.visit_detail vd
+	inner join @personsToDelete p
+		on vd.person_id = p.person_id;
+
+delete vo
+from cdm.visit_occurrence vo
+	inner join @personsToDelete p
+		on vo.person_id = p.person_id;
 
 delete o
 from cdm.observation o
