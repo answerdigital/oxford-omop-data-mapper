2e75b63d6ccc4c8ac3ab46fe26a0c72f9ff18c0e
holtyad <tom.holt@answerdigital.com>
2025-05-06 14:57:51 +0100

    update spell id

--- OmopTransformer/SUS/OP/DeviceExposure/SusOPDeviceExposureRecord.cs
+++ OmopTransformer/SUS/OP/DeviceExposure/SusOPDeviceExposureRecord.cs
@@ -7,7 +7,7 @@ namespace OmopTransformer.SUS.OP.DeviceExposure;
 [SourceQuery("SusOPDeviceExposure.xml")]
 internal class SusOPDeviceExposureRecord
 {
-    public string? HospitalSpellProviderNumber { get; set; }
+    public string? SUSgeneratedspellID { get; set; }
     public string? NHSNumber { get; set; }
     public string? AppointmentDate { get; set; }
     public string? AppointmentTime { get; set; }
