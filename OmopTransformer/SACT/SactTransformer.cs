e9a96b45f20198d4533de864326cc7b93cfd4dcf
holtyad <tom.holt@answerdigital.com>
2025-05-22 16:44:58 +0100

    add condition

--- OmopTransformer/SACT/SactTransformer.cs
+++ OmopTransformer/SACT/SactTransformer.cs
@@ -4,10 +4,11 @@ using OmopTransformer.Omop;
 using OmopTransformer.Omop.Location;
 using OmopTransformer.Omop.Person;
 using OmopTransformer.Omop.DrugExposure;
+using OmopTransformer.Omop.ConditionOccurrence;
 using OmopTransformer.SACT.Person;
 using OmopTransformer.SACT.Location;
 using OmopTransformer.SACT.DrugExposure;
-
+using OmopTransformer.SACT.ConditionOccurrence;
 
 namespace OmopTransformer.SACT;
 internal class SactTransformer : Transformer
@@ -15,6 +16,7 @@ internal class SactTransformer : Transformer
     private readonly ILocationRecorder _locationRecorder;
     private readonly IPersonRecorder _personRecorder;
     private readonly IDrugExposureRecorder _drugExposureRecorder;
+    private readonly IConditionOccurrenceRecorder _conditionOccurrenceRecorder;
 
     public SactTransformer(
         IRecordTransformer recordTransformer,
@@ -24,6 +26,7 @@ internal class SactTransformer : Transformer
         ILocationRecorder locationRecorder,
         IPersonRecorder personRecorder,
         IDrugExposureRecorder drugExposureRecorder,
+        IConditionOccurrenceRecorder conditionOccurrenceRecorder,
         IConceptMapper conceptMapper,
         IRunAnalysisRecorder runAnalysisRecorder)
         : base(
@@ -38,6 +41,7 @@ internal class SactTransformer : Transformer
         _locationRecorder = locationRecorder;
         _personRecorder = personRecorder;
         _drugExposureRecorder = drugExposureRecorder;
+        _conditionOccurrenceRecorder = conditionOccurrenceRecorder;
     }
 
     public async Task Transform(CancellationToken cancellationToken)
@@ -56,9 +60,15 @@ internal class SactTransformer : Transformer
             newId,
             cancellationToken);
 
-        await Transform<SactDrugExposureRecord, SactDrugExposure>(
-            _drugExposureRecorder.InsertUpdateDrugExposure,
-            "SACT Drug Exposure",
+        // await Transform<SactDrugExposureRecord, SactDrugExposure>(
+        //     _drugExposureRecorder.InsertUpdateDrugExposure,
+        //     "SACT Drug Exposure",
+        //     newId,
+        //     cancellationToken);
+
+        await Transform<SactConditionOccurrenceRecord, SactConditionOccurrence>(
+            _conditionOccurrenceRecorder.InsertUpdateConditionOccurrence,
+            "SACT Condition Occurrence",
             newId,
             cancellationToken);
     }
