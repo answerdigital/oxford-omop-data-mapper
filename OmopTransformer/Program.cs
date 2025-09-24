f1e8fef48e0c459df5e35dcf8da42a6098bd0295
James Cockayne <James.Cockayne@answerdigital.com>
2025-09-24 11:41:54 +0100

    Set culture settings.

--- OmopTransformer/Program.cs
+++ OmopTransformer/Program.cs
@@ -1,51 +1,52 @@
-﻿using System.Runtime.CompilerServices;
+﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
-using CommandLine;
-using OmopTransformer.Documentation;
 using OmopTransformer.COSD;
-using OmopTransformer.Transformation;
-using OmopTransformer.Omop.Location;
-using OmopTransformer.SACT;
-using OmopTransformer.SACT.Staging;
 using OmopTransformer.COSD.Staging;
+using OmopTransformer.Documentation;
+using OmopTransformer.Omop;
+using OmopTransformer.Omop.CareSite;
 using OmopTransformer.Omop.ConditionOccurrence;
+using OmopTransformer.Omop.Death;
+using OmopTransformer.Omop.DeviceExposure;
+using OmopTransformer.Omop.DrugExposure;
+using OmopTransformer.Omop.Location;
+using OmopTransformer.Omop.Measurement;
+using OmopTransformer.Omop.Observation;
 using OmopTransformer.Omop.Person;
-using OmopTransformer.RTDS;
-using OmopTransformer.RTDS.Parser;
-using OmopTransformer.Rtds.Staging;
-using OmopTransformer.RTDS.Staging;
+using OmopTransformer.Omop.ProcedureOccurrence;
+using OmopTransformer.Omop.Provider;
 using OmopTransformer.Omop.Prune;
 using OmopTransformer.Omop.VisitDetail;
 using OmopTransformer.Omop.VisitOccurrence;
-using OmopTransformer.Omop.Death;
-using OmopTransformer.Omop.ProcedureOccurrence;
-using OmopTransformer.Omop.DrugExposure;
-using OmopTransformer.Omop.DeviceExposure;
-using OmopTransformer.Omop.Measurement;
-using OmopTransformer.Omop.Observation;
-using OmopTransformer.Omop.CareSite;
-using OmopTransformer.Omop.Provider;
-using OmopTransformer.SUS.APC;
-using OmopTransformer.SUS.OP;
-using OmopTransformer.SUS.AE;
-using OmopTransformer.SUS.Staging.AE;
-using OmopTransformer.SUS.Staging.AE.Clearing;
-using OmopTransformer.SUS.Staging.OP;
-using OmopTransformer.SUS.Staging.OP.Clearing;
-using OmopTransformer.SUS.Staging.Inpatient.APC;
-using OmopTransformer.SUS.Staging.Inpatient.Clearing;
-using OmopTransformer.SUS.Staging.Inpatient;
-using OmopTransformer.SUS.Staging.Inpatient.CCMDS;
-using OmopTransformer.Omop;
 using OmopTransformer.OxfordGP;
 using OmopTransformer.OxfordGP.Staging;
 using OmopTransformer.OxfordGP.Staging.Clearing;
+using OmopTransformer.OxfordPrescribing;
 using OmopTransformer.OxfordPrescribing.Staging;
 using OmopTransformer.OxfordPrescribing.Staging.Clearing;
-using OmopTransformer.OxfordPrescribing;
 using OmopTransformer.OxfordSpineDeath;
+using OmopTransformer.Rtds.Staging;
+using OmopTransformer.RTDS;
+using OmopTransformer.RTDS.Parser;
+using OmopTransformer.RTDS.Staging;
+using OmopTransformer.SACT;
+using OmopTransformer.SACT.Staging;
+using OmopTransformer.SUS.AE;
+using OmopTransformer.SUS.APC;
+using OmopTransformer.SUS.OP;
+using OmopTransformer.SUS.Staging.AE;
+using OmopTransformer.SUS.Staging.AE.Clearing;
+using OmopTransformer.SUS.Staging.Inpatient;
+using OmopTransformer.SUS.Staging.Inpatient.APC;
+using OmopTransformer.SUS.Staging.Inpatient.CCMDS;
+using OmopTransformer.SUS.Staging.Inpatient.Clearing;
+using OmopTransformer.SUS.Staging.OP;
+using OmopTransformer.SUS.Staging.OP.Clearing;
+using OmopTransformer.Transformation;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OmopTransformerTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
@@ -58,6 +59,13 @@ internal class Program
 
     private static async Task Main(string[] args)
     {
+        Console.WriteLine($"Current culture is {CultureInfo.DefaultThreadCurrentCulture}");
+
+        CultureInfo culture = new CultureInfo("en-GB");
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+
+        Console.WriteLine($"Setting culture to {culture}");
+
         var builder = Host.CreateApplicationBuilder(args);
 
         var result = Parser.Default.ParseArguments<StagingOptions, DocumentationOptions, TransformOptions, FinaliseOptions>(args);
